e15ef11f168337b0e0eff7d0267c0bb78c915311
taryn yaeger <tsyaeger@gmail.com>
2018-07-30 02:55:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,8 +16,5 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
   
-  def last_updated
-    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
-  end
-  
+
 end
